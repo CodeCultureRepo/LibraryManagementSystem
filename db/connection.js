07bbca37a5fc4c23cecbb5caf40a416f1501e9e5
CodeCultureRepo <abigailsampson203@yahoo.com>
2024-11-05 12:30:52 +0200

    Add: Database Connection made

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'your_password',
+    user: 'abigails',
+    password: 'G0ldenSpringEngineering!',
     database: 'library_management'
 });
 
